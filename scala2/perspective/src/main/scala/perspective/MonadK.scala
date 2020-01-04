bbac511f9a98dcbdaa1886439a13317acc5b47bc
Katrix <katrix97@hotmail.com>
2020-01-05 00:25:57 +0100

    Add MonadK and RepresentableK

---
+++ scala2/perspective/src/main/scala/perspective/MonadK.scala
@@ -0,0 +1,13 @@
+package perspective
+
+import simulacrum.typeclass
+
+//TODO: Check if this is the valid way to define MonadK
+@typeclass trait MonadK[F[_[_], _]] extends ApplicativeK[F] {
+
+  def flattenK[A[_], C](ffa: F[F[A, *], C]): F[A, C] = flatMapK(ffa)(FunctionK.identity)
+
+  def flatMapK[A[_], B[_], C](fa: F[A, C])(f: A ~>: F[B, *]): F[B, C]
+
+  //TODO: Implement mapK and map2K in terms of flatMapK
+}
---
+++ scala2/perspective/src/main/scala/perspective/RepresentableK.scala
@@ -0,0 +1,26 @@
+package perspective
+
+import cats.Functor
+
+trait RepresentableK[F[_[_], _]] extends MonadK[F] with DistributiveK[F] {
+
+  type RepresentationK[_]
+
+  def indexK[A[_], C](fa: F[A, C]): RepresentationK ~>: A
+
+  def tabulateK[A[_], C](f: RepresentationK ~>: A): F[A, C]
+
+  override def pureK[A[_], C](a: Unit #~>: A): F[A, C] = tabulateK(λ[RepresentationK ~>: A](_ => a(())))
+
+  override def flatMapK[A[_], B[_], C](fa: F[A, C])(f: A ~>: F[B, *]): F[B, C] =
+    tabulateK(λ[RepresentationK ~>: B](a => indexK(f(indexK(fa)(a)))(a)))
+
+  override def cosequenceK[G[_], A[_], C](gfa: G[F[A, C]])(implicit G: Functor[G]): F[Compose2[G, A, *], C] =
+    tabulateK[Compose2[G, A, *], C](λ[RepresentationK ~>: Compose2[G, A, *]](r => G.map(gfa)(indexK(_)(r))))
+
+  override def mapK[A[_], B[_], C](fa: F[A, C])(f: A ~>: B): F[B, C] =
+    tabulateK(λ[RepresentationK ~>: B](r => f(indexK(fa)(r))))
+
+  override def map2K[A[_], B[_], ZT[_], C](fa: F[A, C], fb: F[B, C])(f: Tuple2K[A, B]#λ ~>: ZT): F[ZT, C] =
+    tabulateK(λ[RepresentationK ~>: ZT](r => f((indexK(fa)(r), indexK(fb)(r)))))
+}
